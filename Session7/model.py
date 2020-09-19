de537c22c49a334d69cd4bdf1404081cb94da80a
Sahal Kadankandy <sahal.kadankandy@yahoo.com>
2020-09-19 22:44:22 +1000

    Batch size update

--- Session7/model.py
+++ Session7/model.py
@@ -133,13 +133,13 @@ class Loader(object):
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
+    self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
+                                              shuffle=True, num_workers=4)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                           download=True, transform=transform)
-    self.testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                            shuffle=False, num_workers=2)
+    self.testloader = torch.utils.data.DataLoader(testset, batch_size=64,
+                                            shuffle=False, num_workers=4)
 
     self.classes = ('plane', 'car', 'bird', 'cat',
               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
