ff046e0b088e427fcfb30a18f1b5187474f111d3
Sahal Kadankandy <sahal.kadankandy@yahoo.com>
2020-10-11 01:11:20 +1100

    initial commit S11

---
+++ Session11/data_manager/data_manager_albumentations.py
@@ -0,0 +1,97 @@
+import torchvision
+from torchvision import datasets
+
+import torch
+from albumentations import Compose, Rotate, HorizontalFlip, Normalize, RandomCrop, Cutout, PadIfNeeded, Lambda
+from albumentations.pytorch import ToTensor
+
+class CIFAR10_dataset(datasets.CIFAR10):
+    """
+    Custom class to include albumentations data augmentations
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Constructor for custom CIFAR10 dataset
+        """
+        super().__init__(**kwargs)
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        img, target = self.data[index], self.targets[index]
+
+        # doing this so that it is consistent with all other datasets
+        if self.transform:
+            augmented = self.transform(image=img)
+            img = augmented['image']
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
+class DataManager(object):
+    """
+    Class that handles data management for an experiment
+    """
+    
+    def __init__(self, batch_size=64, use_cuda=True, dataset_name='cifar10', 
+                 trainset = None, testset = None,
+                 train_transforms=None, test_transforms=None):
+        super().__init__()
+
+        self.dataset_name = dataset_name
+
+        if self.dataset_name == 'cifar10':
+            if trainset:
+                self.trainset = trainset
+            else:
+                # Train Phase transformations
+                if train_transforms:
+                    self.train_transforms = train_transforms
+                else:
+                    self.train_transforms = train_transforms = Compose([Lambda(image=DataManager.padded_random_crop, always_apply=False, p=0.5),
+                                                                        Cutout(num_holes=4, max_h_size=8, max_w_size=8, always_apply=False, p=0.5),
+                                                                        HorizontalFlip(p=0.5),
+                                                                        Normalize(
+                                                                            mean=[0.4914, 0.4822, 0.4465],
+                                                                            std=[0.2471, 0.2435, 0.2616],
+                                                                        ),
+                                                                        ToTensor()
+                                                                        ])
+                self.trainset = CIFAR10_dataset(root='./data', train=True, download=True, transform=self.train_transforms)
+            
+            if testset:
+                self.testset = testset
+            else:
+                # Test Phase transformations
+                if test_transforms:
+                    self.test_transforms = test_transforms
+                else:
+                    self.test_transforms = Compose([Normalize(
+                                                        mean=[0.4914, 0.4822, 0.4465],
+                                                        std=[0.2471, 0.2435, 0.2616],
+                                                    ),
+                                                    ToTensor()
+                                                    ])
+                self.testset = CIFAR10_dataset(root='./data', train=False, download=True, transform=self.test_transforms)
+
+            # dataloader arguments - something you'll fetch these from cmdprmt
+            dataloader_args = dict(shuffle=True, batch_size=batch_size, num_workers=4, pin_memory=True) if use_cuda else dict(shuffle=True, batch_size=batch_size)
+
+            # train dataloader
+            self.train_loader = torch.utils.data.DataLoader(self.trainset, **dataloader_args)
+            # test dataloader
+            self.test_loader = torch.utils.data.DataLoader(self.testset, **dataloader_args)
+
+    @staticmethod
+    def padded_random_crop (x , **kwargs):
+        x = PadIfNeeded(min_height=40, min_width=40, border_mode=4, value=None, mask_value=None, always_apply=True).apply(x)
+        x = RandomCrop(height=32, width=32, always_apply=True).apply(x)
+        return x
---
+++ Session11/utils.py
@@ -0,0 +1,17 @@
+import torch
+from torchsummary import summary             # Import summary with pytorch
+from torchviz import make_dot
+from torch.autograd import Variable
+
+def textual_summary_cifar10(model):
+    summary(model, input_size=(3, 32, 32))
+
+def graphical_summary_cifar10(model, use_cuda=True, save=True):
+    random_input = torch.randn(1, 3, 32, 32).cuda() if use_cuda else torch.randn(1, 3, 32, 32) 
+    model.eval()
+    y = model(Variable(random_input))
+    dot_graph = make_dot(y)
+    if save:
+        dot_graph.format = 'svg'
+        dot_graph.render(f'model_architecture')
+    return dot_graph
